b5265548f12a03d08d6c6c266d0522cfed4203d9
Konrad Kihan <konrad.kihan@gmail.com>
2022-06-02 12:49:27 +0200

    implemented reading users data and saving new users

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from MainMenu import MainMenu
 from NewUser import NewUser
-from UserInfo import ClientInfo
+from ClientInfo import GetClientId, ClientInfoShow
 from RemoveUser import RemoveUser
+from DatabaseManager import DatabaseConnect
 
 
 if __name__ == "__main__":
+    databaseConnect = DatabaseConnect()
     while True:
             mainMenu = MainMenu()
             # quit app via clicking X
@@ -13,8 +15,22 @@ if __name__ == "__main__":
             # add new user
             elif mainMenu.optionValue == 100:
                 newUser = NewUser()
+                # getting user data from form
+                # fomrat: dict(name: str, surname: str, address1: str, address2: str)
+                newUserData: dict = newUser.newUserData
+                databaseConnect.insert_into_client(newUserData["name"],
+                                                   newUserData["surname"],
+                                                   newUserData["address1"]+newUserData["address2"])
             # see through user list
             elif mainMenu.optionValue == 200:
-                clientInfo = ClientInfo()
+                while True:
+                    clientId = GetClientId().userId
+                    clientInfo = databaseConnect.select_customer(clientId)
+                    if not ClientInfoShow(clientInfo).doContinue:
+                        break
+
+
             elif mainMenu == 300:
-                removeUser == RemoveUser()
+                removeUser = RemoveUser()
+            else:
+                exit(69)
